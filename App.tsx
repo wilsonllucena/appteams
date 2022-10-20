a46c4c9a42b3ea0583967080a412888bc167c988
Wilson Lucena <wilsonllucena@gmail.com>
2022-10-20 01:51:42 -0300

    feat: personalize fonts in app

--- App.tsx
+++ App.tsx
@@ -1,11 +1,14 @@
 import { ThemeProvider } from "styled-components"; // Para envolver a aplicação com o tema definido
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from "@expo-google-fonts/roboto";
 import theme from "@theme/index";
 import Groups from "@screens/Groups";
+import { ActivityIndicator } from "react-native";
 
 export default function App() {
+ const [ fontsLoaded ] = useFonts({ Roboto_400Regular,Roboto_700Bold });
   return (
     <ThemeProvider theme={theme}>
-      <Groups />
+      { fontsLoaded ? <Groups /> : <ActivityIndicator />}
     </ThemeProvider>
   );
 }
