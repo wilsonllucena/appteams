6f5484b7458279067309f5e274b055d2fbd59966
Wilson Lucena <wilsonllucena@gmail.com>
2022-10-20 01:28:42 -0300

    feat: includes styled-components react

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,18 +1,12 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Text } from 'react-native';
+import { Container } from './styles';
 
 export default function Groups() {
     return (
-        <View style={styles.container}>
+        <Container>
             <Text>Groups</Text>
-        </View>
+        </Container>
     );
 }
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-});
+
