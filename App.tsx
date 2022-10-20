1fdb6ea8181450e29d593bad6bec6fb5b817004c
Wilson Lucena <wilsonllucena@gmail.com>
2022-10-20 02:00:02 -0300

    feat: created component loading

--- App.tsx
+++ App.tsx
@@ -2,13 +2,13 @@ import { ThemeProvider } from "styled-components"; // Para envolver a aplicaçã
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from "@expo-google-fonts/roboto";
 import theme from "@theme/index";
 import Groups from "@screens/Groups";
-import { ActivityIndicator } from "react-native";
+import Loading from "@components/Loading";
 
 export default function App() {
  const [ fontsLoaded ] = useFonts({ Roboto_400Regular,Roboto_700Bold });
   return (
     <ThemeProvider theme={theme}>
-      { fontsLoaded ? <Groups /> : <ActivityIndicator />}
+      { fontsLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
   );
 }
