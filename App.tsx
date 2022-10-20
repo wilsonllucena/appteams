654f6e6e2bf3b371d87569515c8348403e187401
Wilson Lucena <wilsonllucena@gmail.com>
2022-10-20 02:02:08 -0300

    feat: configurations statusbars app

--- App.tsx
+++ App.tsx
@@ -3,12 +3,18 @@ import { useFonts, Roboto_400Regular, Roboto_700Bold } from "@expo-google-fonts/
 import theme from "@theme/index";
 import Groups from "@screens/Groups";
 import Loading from "@components/Loading";
+import { StatusBar } from "react-native";
 
 export default function App() {
- const [ fontsLoaded ] = useFonts({ Roboto_400Regular,Roboto_700Bold });
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
   return (
     <ThemeProvider theme={theme}>
-      { fontsLoaded ? <Groups /> : <Loading />}
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
   );
 }
