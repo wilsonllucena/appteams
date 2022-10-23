1bca29f5588136bfe354e2ccb1f864d61f69f7d1
Wilson Lucena <wilsonllucena@gmail.com>
2022-10-23 20:55:17 -0300

    feat: creaty component listempty

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,14 +1,26 @@
 import GroupCard from '@components/GroupCard';
 import Header from '@components/Header';
 import Highlight from '@components/Highlight';
+import ListEmpty from '@components/ListEmpty';
+import { useState } from 'react';
+import { FlatList } from 'react-native';
 import { Container } from './styles';
 
 export default function Groups() {
+    const [groups, setGroups] = useState<string[]>([])
     return (
         <Container>
             <Header  />
             <Highlight title='Turmas' subtitle='Jogue com a sua turma' />
-            <GroupCard title='Galera do time' />
+            <FlatList 
+                data={groups}
+                keyExtractor={item => item}
+                renderItem={({item}) => (
+                    <GroupCard title={item} />
+                )}
+                contentContainerStyle={ Groups.length === 0 && { flex: 1}}
+                ListEmptyComponent={() => (<ListEmpty message='Que tal cadastrar a primeira turma'/>)}
+            />
         </Container>
     );
 }
